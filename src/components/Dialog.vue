3129d6bf06bf3f082ab72c4b58f818b6a8b8fe59
azadra527@gmail.com <azadra527@gmail.com>
2022-02-20 19:07:00 +0700

    add dialog

---
+++ src/components/Dialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <keep-alive>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      persistent
+      transition="dialog-bottom-transition"
+      :retain-focus="false"
+    >
+      <component :is="currentComponent" @closed="setDialogStatus"></component>
+    </v-dialog>
+  </keep-alive>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  components: {
+    Login: () => import("./Login.vue"),
+    Register: () => import("./Register.vue"),
+    Tambahblog: () => import("./TambahBlog.vue"),
+    Editblog: () => import("./EditBlog.vue"),
+    Uploadphoto: () => import("./Upload.vue"),
+  },
+
+  computed: {
+    ...mapGetters({
+      dialogStatus: "dialog/status",
+      currentComponent: "dialog/component",
+    }),
+
+    dialog: {
+      get() {
+        return this.dialogStatus;
+      },
+      set(value) {
+        this.setDialogStatus(value);
+      },
+    },
+  },
+
+  methods: {
+    ...mapActions({
+      setDialogStatus: "dialog/setStatus",
+    }),
+  },
+};
+</script>
