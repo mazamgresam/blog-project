b12405309b54ca0e8c2662e69eed4ca7446efad3
azadra527@gmail.com <azadra527@gmail.com>
2022-02-23 11:08:54 +0700

    add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div block class="pa-10 text-center">
+    <img src="../assets/azam.jpg" class="rounded-circle pa-4" />
+    <h3 class="text-center">M. Azam Gresa Mahendra</h3>
+    <h3 class="text-center">azadra527gmail.com</h3>
+    <div class="pa-6 text-left">
+      <p class="mb-2">
+        I am a fresh graduate from the faculty of Telecommunication Engineer in
+        Bandung State Polytechnic with more than 1 year experience in Network
+        Communication Systems, Electronics Circuits and Modules, IOT, and Radio
+        Frequency (RF) Communication.
+      </p>
+      <p class="mb-2">
+        In addition, I also have the ability in the field of Web Programmers,
+        especially Front End Developer with less than 1 year experience.
+      </p>
+      <p>
+        I am an honest person, disciplined, Hardworking, Creative, and have high
+        motivation in making my dreams come true.
+      </p>
+    </div>
   </div>
 </template>
